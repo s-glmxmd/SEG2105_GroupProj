b0e7003ea1260416bf97e4d5b68c0a9d131b5013
Colin Peters <cpete040@uottawa.ca>
2018-10-25 16:50:21 -0400

    Copied over Dirieh's work to new master

--- Deliverable1/app/src/main/java/com/ahhhh/deliverable1/EnterAccountCridentials.java
+++ Deliverable1/app/src/main/java/com/ahhhh/deliverable1/EnterAccountCridentials.java
@@ -2,6 +2,8 @@ package com.ahhhh.deliverable1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class EnterAccountCridentials extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class EnterAccountCridentials extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_account_cridentials);
+    }
+    public void onSubmitClick(View view){
+        String firstName = ((EditText)findViewById(R.id.firstNameEditText)).getText().toString();
+        String lastName = ((EditText)findViewById(R.id.lastNameEditText)).getText().toString();
+        String username = ((EditText)findViewById(R.id.usernameEditText)).getText().toString();
+        String password = ((EditText)findViewById(R.id.passwordEditText)).getText().toString();
+
+        UserAccount account = new UserAccount(username,password,firstName,lastName);
+
+        if(!account.validateEnteredInfo().equals("")){
+            ((EditText)findViewById(R.id.feedbackTextView)).setText(account.validateEnteredInfo());
+        } else{
+            //go to welcome page
+            //
+        }
     }
 }
--- Deliverable1/app/src/main/java/com/ahhhh/deliverable1/UserAccount.java
+++ Deliverable1/app/src/main/java/com/ahhhh/deliverable1/UserAccount.java
@@ -1,5 +1,7 @@
 package com.ahhhh.deliverable1;
 
+import java.util.regex.Pattern;
+
 public class UserAccount {
     private String username, password, firstName, lastName;
 
@@ -8,5 +10,21 @@ public class UserAccount {
         this.password=password;
         this.firstName=firstName;
         this.lastName=lastName;
+    }
+
+    public String validateEnteredInfo(){
+        String regx = "^[\\p{L} .'-]+$";
+        Pattern p = Pattern.compile(regx, Pattern.CASE_INSENSITIVE);
+        if (firstName.equals("") || lastName.equals("") || username.equals("") || password.equals("")){
+            return "Text inputs cannot be empty!";
+        } else if (!p.matcher(firstName).find()){
+            return ("Invalid First name \"" + firstName + "\"");
+        } else if (!p.matcher(lastName).find()){
+            return ("Invalid Last name \"" + lastName + "\"");
+        } else if (password.equals("") || password.length() <= 4 ){
+            return ("Password size must be a greater than 4 characters, ");
+        }
+
+        return "";
     }
 }
