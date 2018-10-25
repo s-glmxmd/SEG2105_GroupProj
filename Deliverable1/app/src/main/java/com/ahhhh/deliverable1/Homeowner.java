ee55ff6c9321daba34a165dbdde6f78cdf063a62
Colin Peters <cpete040@uottawa.ca>
2018-10-25 00:00:21 -0400

    Added domain hierarchy & login functionality

---
+++ Deliverable1/app/src/main/java/com/ahhhh/deliverable1/Homeowner.java
@@ -0,0 +1,9 @@
+package com.ahhhh.deliverable1;
+
+public class Homeowner extends UserAccount {
+
+    public Homeowner(String username, String password, String firstName, String lastName){
+        super(username,password,firstName,lastName);
+    }
+
+}
