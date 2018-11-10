641742c2416384167ef94a3d115dc47ae7996e9d
s-glmxmd <smaxa028@uottawa.ca>
2018-11-10 00:28:23 -0500

    removed Deliv1 forlder with second copy of app

--- Deliv1/app/src/main/java/com/ahhhh/deliv1/Administrator.java
+++
@@ -1,7 +0,0 @@
-package com.ahhhh.deliv1;
-
-public class Administrator extends UserAccount {
-    public Administrator(String username, String password, String firstName, String lastName){
-        super(username,password,firstName,lastName);
-    }
-}
--- Deliv1/app/src/main/java/com/ahhhh/deliv1/Homeowner.java
+++
@@ -1,7 +0,0 @@
-package com.ahhhh.deliv1;
-
-public class Homeowner extends UserAccount {
-    public Homeowner(String username, String password, String firstName, String lastName){
-        super(username,password,firstName,lastName);
-    }
-}
