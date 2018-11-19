7fd5358241bbcb6cbfe13857f5fbf87df7e6370d
Colin Peters <cpete040@uottawa.ca>
2018-11-19 00:41:32 -0500

    Completed profile creation for SP, DB interaction incomplete.

--- app/src/main/java/com/ahhhh/deliv1/ServiceProvider.java
+++ app/src/main/java/com/ahhhh/deliv1/ServiceProvider.java
@@ -3,15 +3,19 @@ package com.ahhhh.deliv1;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 public class ServiceProvider extends UserAccount {
 
     private ArrayList<Service> services;
     private ArrayList<Timestamp> availabilities;
+
     private String companyName;
-    private boolean isLicenced = false;
     private ProfileAddress workerAddress;
     private String phoneNumber;
+    private boolean isLicenced = false;
+    private String description;
+
 
     public ServiceProvider(String username, String password, String firstName, String lastName){
         super(username,password,firstName,lastName);
@@ -49,9 +53,33 @@ public class ServiceProvider extends UserAccount {
         isLicenced = val;
     }
 
+    public boolean isLicenced() {
+        return isLicenced;
+    }
+
     public void setCompanyName(String cName) {
         companyName = cName;
     }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public String validateProfile(){
+        String error = workerAddress.validate();
+
+        if (companyName.equals("") || phoneNumber.equals("")){
+            error = "Mandatory inputs cannot be empty!";
+        }
+        if (phoneNumber.matches(".*[a-zA-Z]+.*")){
+            error = "Phone number cannot contain letters";
+        }
+
+        return error;
+    }
+
+
+
 
 
 
