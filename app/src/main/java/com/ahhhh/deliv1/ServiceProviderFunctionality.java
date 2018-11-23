695c6b753f2f852f6c31478dccd01407e1da1e86
s-glmxmd <smaxa028@uottawa.ca>
2018-11-22 22:34:05 -0500

    Reverting branch copy to state before pull from master
    Missing the availabilities for a service provider

--- app/src/main/java/com/ahhhh/deliv1/ServiceProviderFunctionality.java
+++ app/src/main/java/com/ahhhh/deliv1/ServiceProviderFunctionality.java
@@ -1,5 +1,6 @@
 package com.ahhhh.deliv1;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ServiceProviderFunctionality extends AppCompatActivity {
     private String username;
@@ -27,31 +29,19 @@ public class ServiceProviderFunctionality extends AppCompatActivity {
         String [] name = myDBHelper.getName(username);
         String password = myDBHelper.findPassword(username);
 
-        //fill profile fields on create
+        //fill profile fields
         tempSP = new ServiceProvider(username,password,name[0],name[1]);
-        //FEMALE ** SET tempSP EQUAL TO THE SERVICE PROVIDER ASSOCIATED WITH USERNAME (not the test instance i've made above)
+
 
         ((TextView)findViewById(R.id.txtUsername)).setText(tempSP.getUsername());
         ((TextView)findViewById(R.id.txtFirstName)).setText(tempSP.getFirstName());
         ((TextView)findViewById(R.id.txtLastName)).setText(tempSP.getLastName());
 
-
-        //if mandatory fields have not been filled, make the only option available create profile
-        if(tempSP.getWorkerAddress()==null){
-            ViewGroup profileInfo = findViewById(R.id.layoutProfileInfo);
-            profileInfo.removeAllViews();
-        } else{
-            Button createProfileBtn = findViewById(R.id.btnCreateProfile);
-            ((ViewGroup) (createProfileBtn).getParent()).removeView(createProfileBtn);
-        }
-        ((TextView)findViewById(R.id.txtStreetAddress)).setText(tempSP.getWorkerAddress().getStreetAddress());
-        ((TextView)findViewById(R.id.txtProvince)).setText(tempSP.getWorkerAddress().getProvince());
-        ((TextView)findViewById(R.id.txtCountry)).setText(tempSP.getWorkerAddress().getCountry());
-        ((TextView)findViewById(R.id.txtPostal)).setText(tempSP.getWorkerAddress().getPostalCode());
-        ((TextView)findViewById(R.id.txtCompany)).setText(tempSP.getCompanyName());
-        ((TextView)findViewById(R.id.txtPhoneNumber)).setText(tempSP.getPhoneNumber());
-
-
+        Context context = getApplicationContext();
+        CharSequence message = "Please complete your profile information!";
+        int duration = Toast.LENGTH_LONG;
+        Toast toast = Toast.makeText(context, message, duration);
+        toast.show();
 
     }
 
@@ -64,15 +54,5 @@ public class ServiceProviderFunctionality extends AppCompatActivity {
 
 
 
-
-    public void btnEditProfileClick(View view){
-        btnCreateProfileClick(view);
-    }
-
-    public void setAvailibilityButton(View view){
-        //Intent i =new Intent(ServiceProviderFunctionality.this, ServiceProviderAvailability.class);
-        //startActivity(i);
-
-    }
 
 }
