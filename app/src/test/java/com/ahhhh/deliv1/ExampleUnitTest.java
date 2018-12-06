fe1b9de5b3b6ab8c47f82ee991460c2ac5f9d330
Dirieh <diriehmahdi@gmail.com>
2018-12-06 00:01:52 -0500

    Unit testing changes

--- app/src/test/java/com/ahhhh/deliv1/ExampleUnitTest.java
+++ app/src/test/java/com/ahhhh/deliv1/ExampleUnitTest.java
@@ -25,4 +25,98 @@ public class ExampleUnitTest {
         s1.setLicenced(true);
         assertEquals("Actual vs expected", true, s1.isLicenced());
     }
+
+    @Test
+    public void rating_test() {
+        Rating rating1 = new Rating(5, "Very good service provider.");
+        int numRating = rating1.getNumericRating();
+        assertEquals("Actual vs expected", numRating, 5);
+    }
+
+    @Test
+    public void rating_test2() {
+        Rating rating1 = new Rating(5, "Very good service provider.");
+        String comment = rating1.getComment();
+        assertEquals("Actual vs expected", comment, "Very good service provider.");
+    }
+
+    @Test
+    public void rating_test3() {
+        Rating rating1 = new Rating(5, "Very good service provider.");
+        rating1.setNumericRating(4);
+        assertEquals("Actual vs expected", rating1.getNumericRating(), 4);
+
+        //setComment
+    }
+
+    @Test
+    public void rating_test4() {
+        Rating rating1 = new Rating(5, "Very good service provider.");
+        String res = rating1.validateRating();
+        assertEquals("Actual vs expected", res, "");
+        //validateRating
+
+    }
+
+    @Test
+    public void rating_test5() {
+        Rating rating1 = new Rating(555555563, "Very good service provider.");
+        String res = rating1.validateRating();
+        assertEquals("Actual vs expected", res, "Please rate the service between 1 and 5 (inclusive)");
+
+        //validateRating
+
+    }
+
+
+    @Test
+    public void booking_test() {
+        Booking booking = new Booking("saturday", null);
+        String day = booking.getWeekday();
+        assertEquals("Actual vs expected", day, "saturday");
+    }
+
+    @Test
+    public void booking_test2() {
+        Booking booking = new Booking("saturday", null);
+        Service service = booking.getService();
+        assertEquals("Actual vs expected", service, null);
+    }
+
+    @Test
+    public void booking_test3() {
+        Booking booking = new Booking("saturday", null);
+        Rating rating1 = new Rating(5, "Very good service provider.");
+        booking.setHomeownerRating(rating1);
+        Service service = booking.getService();
+        assertEquals("Actual vs expected", booking.getHomeownerRating(), rating1);
+    }
+
+    @Test
+    public void booking_test4() {
+        Booking booking = new Booking("saturday", null);
+        Rating rating1 = new Rating(5, "Very good service provider.");
+        booking.setHomeownerRating(null);
+        Service service = booking.getService();
+        assertEquals("Actual vs expected", booking.getHomeownerRating(), null);
+    }
+
+    @Test
+    public void booking_test5() {
+        Booking booking = new Booking(null, null);
+        String res = booking.getWeekday();
+        assertEquals("Actual vs expected", res, null);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
